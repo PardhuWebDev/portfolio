b66bf865e2569553ba5f74eec99e2cf053cc83e8
Pardha Saradhi <pardhu.sridhar@gmail.com>
2026-05-23 15:23:23 +0530

    Fix domains merge conflict in next config

--- next.config.js
+++ next.config.js
@@ -7,12 +7,8 @@ const nextConfig = {
       'blend-playlist-covers.spotifycdn.com',
       'image-cdn-fa.spotifycdn.com',
       'miro.medium.com',
-<<<<<<< HEAD
       'cdn-images-1.medium.com',
       'upload.wikimedia.org'
-=======
-      'cdn-images-1.medium.com'
->>>>>>> cd09fbcb131a00886d5a242a4e52696754e7ec6b
     ],
     unoptimized: false
   }
